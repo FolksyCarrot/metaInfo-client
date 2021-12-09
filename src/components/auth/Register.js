24930d54c8d6b9bf998278715dd3e007baa6c2c7
Keith Parks <keithparks20@gmail.com>
2021-12-09 15:27:56 -0600

    updated register form to take user first and last name

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -29,7 +29,7 @@ export const Register = (props) => {
                         .then(res => res.json())
                         .then(createdUser => {
                             if (createdUser.hasOwnProperty("id")) {
-                                localStorage.setItem("stock_customer", createdUser.id)
+                                localStorage.setItem("meta_customer", createdUser.id)
                                 history.push("/")
                             }
                         })
@@ -70,18 +70,26 @@ export const Register = (props) => {
             <form className="form--login" onSubmit={handleRegister}>
                 <h1 className="h3 mb-3 font-weight-normal" id="please--register">Please Register</h1>
                 <fieldset>
-                    <label htmlFor="name"> User Name </label>
+                    <label htmlFor="username"> User Name </label>
                     <input onChange={updateManager}
-                           type="text" id="name" className="form-control"
+                           type="text" id="username" className="form-control"
                            placeholder="Enter a user name" required autoFocus />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="address"> Address </label>
-                    <input onChange={updateManager} type="text" id="address" className="form-control" placeholder="Street address" required />
                 </fieldset>
                 <fieldset>
                     <label htmlFor="email"> Email address </label>
                     <input onChange={updateManager} type="email" id="email" className="form-control" placeholder="Email address" required />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="password"> Password </label>
+                    <input onChange={updateManager} type="text" id="password" className="form-control" placeholder="password" required />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="first_name"> First Name </label>
+                    <input onChange={updateManager} type="text" id="first_name" className="form-control" placeholder="password" required />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="last_name"> Last Name </label>
+                    <input onChange={updateManager} type="text" id="last_name" className="form-control" placeholder="password" required />
                 </fieldset>
                 <select  id ="store_id" onChange = {updateManager}>
                     <option>select company</option>
